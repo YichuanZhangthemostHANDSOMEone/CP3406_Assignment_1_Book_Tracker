b646e8018b7d2f20c4edbce2c3bde12b24520123
YichuanZhangthemostHANDSOMEone <Yichuan.Zhang@my.jcu.edu.au>
2025-02-12 22:03:21 +0800

    Add recommend book name and image indicator for recommend book detail page

--- app/src/main/java/com/example/assignment_1booktracker/MainActivity4.kt
+++ app/src/main/java/com/example/assignment_1booktracker/MainActivity4.kt
@@ -1,20 +1,151 @@
 package com.example.assignment_1booktracker
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.assignment_1booktracker.ui.theme.Assignment_1BookTrackerTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
-class MainActivity4 : AppCompatActivity() {
+class MainActivity4 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main3)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // 确保 enableEdgeToEdge() 方法存在
+        // enableEdgeToEdge()
+        setContent {
+            Assignment_1BookTrackerTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    BookInfo(modifier = Modifier.padding(innerPadding))
+                }
+            }
         }
     }
 }
+
+data class RecommendedBook(
+    val image: Painter,
+    val name: String,
+)
+
+@Composable
+fun BookInfo(modifier: Modifier = Modifier) {
+    val book = RecommendedBook(
+            image = painterResource(id = R.drawable.wolf_hall_historical_fiction),
+            name = "Wolf Hall",
+        )
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        item {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+        item {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                BookCard(modifier = Modifier, book = book)
+            }
+        }
+        item {
+            Spacer(modifier = Modifier.height(10.dp))
+        }
+    }
+}
+
+
+@Composable
+fun BookCard(modifier: Modifier = Modifier, book: RecommendedBook) {
+    Card(
+        modifier = modifier
+            .padding(8.dp)
+            .width(190.dp)
+            .height(320.dp),
+    elevation = CardDefaults.cardElevation(5.dp),
+    colors = CardDefaults.cardColors(containerColor = Color.Yellow)
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.9f)
+            ) {
+                Image(
+                    painter = book.image,
+                    contentDescription = "Card Image",
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.1f)
+                    .padding(horizontal = 6.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = book.name,
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.offset(y = (-4).dp)
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun ReasonPreview() {
+    Assignment_1BookTrackerTheme {
+        BookInfo()
+    }
+}
+
